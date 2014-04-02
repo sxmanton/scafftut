4ec7f02e8ab798632325d1218ef3aa0491c1cc57
Sean Manton <sxmanton@bu.edu>
2014-04-02 00:28:19 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Scafftut::Application.config.secret_key_base = '82198936fcfe841bcb4f862098d6260055f343b3f80fd5ee7506c7041de4582654b37193d36e89af4791e1052ab6f09476cff60da8d9091318884dddc05c4ad9'
